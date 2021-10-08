60f93034963f0928411840cfffd7c5a0737dfa04
Tibeau <45169641+Tibeau@users.noreply.github.com>
2021-10-08 11:48:35 +0200

    first commit

---
+++ src/main/java/fact/it/brankedge/BrankEdgeApplication.java
@@ -0,0 +1,13 @@
+package fact.it.brankedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrankEdgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrankEdgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/brankedge/BrankEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.brankedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrankEdgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
