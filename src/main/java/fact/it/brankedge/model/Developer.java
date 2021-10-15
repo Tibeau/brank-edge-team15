21959152398cbe6989cbc4d852baf6528e8ee9ed
Tibeau <45169641+Tibeau@users.noreply.github.com>
2021-10-15 12:22:27 +0200

    developer added

--- src/main/java/fact/it/brankedge/model/Developer.java
+++ src/main/java/fact/it/brankedge/model/Developer.java
@@ -2,5 +2,43 @@ package fact.it.brankedge.model;
 
 public class Developer {
     private int id;
+
     private String name;
+    private int count_workers;
+    private int founding_year;
+
+    public Developer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount_workers() {
+        return count_workers;
+    }
+
+    public void setCount_workers(int count_workers) {
+        this.count_workers = count_workers;
+    }
+
+    public int getFounding_year() {
+        return founding_year;
+    }
+
+    public void setFounding_year(int founding_year) {
+        this.founding_year = founding_year;
+    }
 }
